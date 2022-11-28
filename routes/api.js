644c819bcf10f34cf8ea10ea3c81f57b1c843edf
dodolereporter <dodolereporter@gmail.com>
2022-11-28 16:58:39 +0100

    Ajout Nodeamon + port en 8080 + npm test pour démarer en preprod + npm start pour demarer en prod + TEST

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("API chargée");
+    res.send("API chargée v2");
     console.log("Page API chargée avec succès")
 })
 module.exports = router;
