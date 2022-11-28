d469cb6d2531591d81106999fe3137b41c34a665
ZDashinGC <cguerrero9769230@gmail.com>
2022-11-28 16:38:50 +0100

    ajout d'une nouvelle route

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const app = express()
 const port = 80
 
 const indexRoute = require("./routes/index")
+const apiRoute = require("./routes/api")
 
 app.use("/", indexRoute)
+app.use("/api", apiRoute)
 
 app.listen(port);
 console.log("Application démarée sur le port :" + port)
---
+++ routes/api.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("Hello World 2");
+    console.log("Page API chargée avec succès")
+})
+module.exports = router();
